fdadf1132bdced5e0cf346511ad4df24c4e0b61e
Gustavo Correia <gucorreia2901@gmail.com>
2024-07-28 14:33:40 -0300

    Derpartment entity class

---
+++ src/Application/App.java
@@ -0,0 +1,17 @@
+package Application;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.DbException;
+import model_entities.Department;
+
+
+public class App {
+
+     Department obj = new Department(1,"DEPARTAMENTO DE LIVROS");
+     
+}
+
