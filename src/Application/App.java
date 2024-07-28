956f035b3e28833fd6fde604870d61162338b969
Gustavo Correia <gucorreia2901@gmail.com>
2024-07-28 17:07:28 -0300

    Reutilizando a instanciacao

--- src/Application/App.java
+++ src/Application/App.java
@@ -16,6 +16,8 @@ public static void main(String[] args) {
     
     SellerDao sellerDao = DaoFactory.createSellerDao();
 
+    System.out.println("=== TESTE1: seller findById =====");
+
     Seller seller = sellerDao.findById(3);
 
     System.out.println(seller);
--- src/model_dao_impl/SellerDaoJDBC.java
+++ src/model_dao_impl/SellerDaoJDBC.java
@@ -12,30 +12,29 @@ import model_dao.SellerDao;
 import model_entities.Department;
 import model_entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
 
     private Connection conn;
 
-    public SellerDaoJDBC(Connection conn){
+    public SellerDaoJDBC(Connection conn) {
         this.conn = conn;
     }
 
-
     @Override
     public void insert(Seller obj) {
-        
+
         throw new UnsupportedOperationException("Unimplemented method 'insert'");
     }
 
     @Override
     public void update(Seller obj) {
-        
+
         throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 
     @Override
     public void deleteById(Integer id) {
-        
+
         throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
     }
 
@@ -45,41 +44,48 @@ public class SellerDaoJDBC implements SellerDao{
         ResultSet rs = null;
         try {
             st = conn.prepareStatement("SELECT seller.*, department.Name as DepName "
-            + "FROM seller INNER JOIN department "
-            + "ON seller.DepartmentId = department.Id "
-            + "WHERE seller.Id = ?");
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE seller.Id = ?");
             st.setInt(1, id);
             rs = st.executeQuery();
-            if(rs.next()){
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setNome(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartment(dep);
+            if (rs.next()) {
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instantiateSeller(rs, dep);
                 return obj;
             }
             return null;
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        }
-        finally{
+        } finally {
             DB.CloseResultset(rs);
             DB.CloseStatement(st);
         }
 
     }
 
-    @Override
-    public List<Seller> findAll() {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setNome(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
     }
 
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
+    }
 
+    @Override
+    public List<Seller> findAll() {
+
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
 
 }
