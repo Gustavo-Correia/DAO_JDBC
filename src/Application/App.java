0e4d3333d5c14b7db01e763cc26bce9329aeb348
Gustavo Correia <gucorreia2901@gmail.com>
2024-07-28 17:23:10 -0300

    FindByDepartment hashmap implemation

--- src/Application/App.java
+++ src/Application/App.java
@@ -3,11 +3,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import db.DB;
 import db.DbException;
 import model_dao.DaoFactory;
 import model_dao.SellerDao;
+import model_entities.Department;
 import model_entities.Seller;
 
 
@@ -22,6 +24,15 @@ public static void main(String[] args) {
 
     System.out.println(seller);
 
+    System.out.println("=== TESTE2: seller findByDepartment =====");
+
+    Department department = new Department(2, null);
+
+    List<Seller> list = sellerDao.findByDepartment(department);
+
+    for (Seller seller2 : list) {
+        System.out.println(seller2);
+    }
 }
      
 }
